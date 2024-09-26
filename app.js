38607c57ac27ed01e74b8a0c9d73933543f0f4a6
Exploring-glitch <deysasha262@gmail.com>
2024-09-26 19:09:54 +0530

    made changes

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ x.forEach((button) => {
     console.log(button)
     button.addEventListener ("click",() => {
         console.log("button was clicked");
-        if(turnX === true) {  // for player X
+        //whichplayerturn=turnX?"playerX":"playerO" (USING TERNARY OPERATOR FOR THE SAME)
+           
+        if(turnX) {                             
             button.innerText = "X";
             turnX = false;
             button.disabled = true; // no change after clicking for the 1st time
@@ -31,7 +33,7 @@ x.forEach((button) => {
             turnX = true;
             button.disabled = true; // no change after clicking for the 1st time
         }
-        checkingWinner(grid); // function calling (after each click it checks the winning pattern)
+        checkingWinner(grid); // function calling (after each click it checks the winning pattern
     })    
 });
 
@@ -39,27 +41,41 @@ x.forEach((button) => {
 const checkingWinner = (x) => {
     console.log(typeof x)
     x.forEach(btn=>console.log(btn.innerText))
+
+    
     for (let pattern of winningPaterns ) { 
-        console.log(pattern) // why only 2 partterns
+        console.log(pattern) 
         // first value of pattern will be [0,1,2] 
         // pattern[0]=0// pattern[1]=1// pattern[2]=2
         let a = x[pattern[0]].innerText; // 0th position of a pattern example- patttern is [1,4,7] so a=1
         let b = x[pattern[1]].innerText; // 1st position of a pattern 
         let c = x[pattern[2]].innerText; // 2nd position of a pattern   
         console.log(JSON.stringify([a,b,c])) 
+
         if(JSON.stringify([a,b,c])==JSON.stringify(['X','X','X'])){
 
             let msz=document.getElementById("winnertext")
-            msz.innerText="X is winner";
-                msz.classList.toggle("hidden")
+            msz.innerText="Congratulations! X is winner";
+            msz.classList.toggle("hidden")   
         }
+
+
         if(JSON.stringify([a,b,c])==JSON.stringify(['O','O','O'])){
 
             let msz=document.getElementById("winnertext")
-            msz.innerText="O is winner";
-                msz.classList.toggle("hidden")
+            msz.innerText = "Congratulations! O is winner";
+            msz.classList.toggle("hidden")
         }
+
+        //else{
+            //let msz=document.getElementById("winnertext")
+            //msz.innerText = "DRAW";
+           // msz.classList.toggle("hidden")
+        //}
+    }
             
-}}
+}
     
+
+
 
